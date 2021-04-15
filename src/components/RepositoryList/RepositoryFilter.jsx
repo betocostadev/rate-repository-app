35a9941ba9c7ce2761ed4da5985a7e28b567b24d
betocostadev <robertomarx@gmail.com>
2021-04-15 09:06:59 -0300

    added rnpicker

--- src/components/RepositoryList/RepositoryFilter.jsx
+++ src/components/RepositoryList/RepositoryFilter.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableHighlight } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 import Text from '../Shared/Text';
 
@@ -11,18 +12,27 @@ const styles = StyleSheet.create({
 
 const RepositoryFilter = () => {
   const [filter, setFilter] = useState('Latest repositories');
+  const [selectedFilter, setSelectedFilter] = useState();
 
-  const openFilterMenu = () => {
-    console.log('menu open')
-  }
 
   return (
     <View style={styles.container}>
-      <TouchableHighlight style={{padding: 10, borderRadius: 10}} activeOpacity={0.6} underlayColor="#eeeeee" onPress={openFilterMenu}>
-        <Text fontWeight="bold" fontSize="subheading">{filter}</Text>
-      </TouchableHighlight>
+    <Text>Select an item...</Text>
+      <Picker
+        selectedValue={selectedFilter}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedFilter(itemValue)
+        }>
+        <Picker.Item label="Latest repositories" value="CREATED AT" />
+        <Picker.Item label="Highest rated repositories" value="ASC" />
+        <Picker.Item label="Lowest rated repositories" value="DES" />
+      </Picker>
     </View>
   );
 };
 
 export default RepositoryFilter;
+
+// <TouchableHighlight style={{padding: 10, borderRadius: 10}} activeOpacity={0.6} underlayColor="#eeeeee" onPress={openFilterMenu}>
+//   <Text fontWeight="bold" fontSize="subheading">{filter}</Text>
+// </TouchableHighlight>
